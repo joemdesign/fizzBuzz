2542452d91fac0e35e2937e419cb42889268291b
Joe Morris <joem@joemdesign.com>
2014-05-12 11:21:12 -0400

    New .H & .M files
    Including new supporting files.

--- fizzBuzz/main.m
+++ fizzBuzz/main.m
@@ -7,6 +7,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "console.h"
+
 
 int main(int argc, const char * argv[])
 {
@@ -14,7 +16,25 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+//        int input;
+//        input = getDecimalFromConsole(@"Enter a number");
+//        NSLog(@"The number you entered is %i", input);
+        for (int i = 1; i <10; i++) {
+            if(i % 5) {
+                NSLog(@"%i",i);
+            }else{
+                 NSLog(@"buzz");
+            }
+            if (i % 3){
+                NSLog(@"%i",i);
+            }else{
+                NSLog(@"fizz");
+            }
+            
+//            }else if (i % 3){
+//                NSLog(@"%i", i);
+//            }
+        }
         
     }
     return 0;
