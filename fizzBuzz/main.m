4fd5e57d92fba95694476faf0d48c4e10927e0c0
Joe Morris <joem@joemdesign.com>
2014-05-12 21:58:10 -0400

    FizzBuzz Complete
    Interesting inclusion of “printf (“Fizz\n”); etc. Omitted NSLog which
    now seems bulky.

--- fizzBuzz/main.m
+++ fizzBuzz/main.m
@@ -15,26 +15,19 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-//        int input;
-//        input = getDecimalFromConsole(@"Enter a number");
-//        NSLog(@"The number you entered is %i", input);
-        for (int i = 1; i <10; i++) {
-            if(i % 5) {
-                NSLog(@"%i",i);
-            }else{
-                 NSLog(@"buzz");
+
+        for (int i = 1; i<=30; i++) {
+            if (i % 15 == 0) {
+                printf("FizzBuzz\n");
+            }else if (i % 3 ==0){
+                    printf("Fizz\n");
+                }else if (i % 5 ==0){
+                    printf("Buzz\n");
+            }else {
+                printf("%i\n", i);
             }
-            if (i % 3){
-                NSLog(@"%i",i);
-            }else{
-                NSLog(@"fizz");
-            }
-            
-//            }else if (i % 3){
-//                NSLog(@"%i", i);
-//            }
         }
+        
         
     }
     return 0;
